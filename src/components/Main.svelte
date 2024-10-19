2530c4666244b7c40b84c837cbab42caa3380039
Anurag <hamsen3880@gmail.com>
2024-10-19 22:46:07 +0530

    Main section V0.4

---
+++ src/components/Main.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-xl sm:text-2xl md:text-3xl">
+                Hi! I am <span class="text-violet-400"> Anurag </span> Bhattacharjee <br>
+                Software <span class="text-violet-400"> Developer </span> from 
+                <span class="text-orange-600">In</span>d<span class="text-green-600">ia</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                <span class="text-violet-400"> Working with </span> Javascript | Python | C++
+            </p>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" href="#" target="_blank">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+                <h4 class="relative z-9"> Get In Touch &rarr; </h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/port-removebg-preview.png"} alt="Profile Image" class="object-cover z-[2] max-h-[70vh]" />
+        </div>
+    </section>
+    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                Projects I have worked upon
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Wanna have <span class="text-violet-400"> a look </span> at my works.
+            </h3>
+        </div>
+    </section>
+</main>
